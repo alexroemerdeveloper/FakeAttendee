6ba072230183678e2538d8baa2a3d94590edb95e
Alexander Römer <alexroemerdeveloper@gmail.com>
2021-04-13 14:30:12 +0200

    Initial Commit

---
+++ FakeAttendee/FakeAttendeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeAttendeeApp.swift
+//  FakeAttendee
+//
+//  Created by Alexander Römer on 13.04.21.
+//
+
+import SwiftUI
+
+@main
+struct FakeAttendeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
